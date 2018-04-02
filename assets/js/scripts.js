a7b3278fe36b01625b6ed8557fb8648b88840bd1
Mariana Silva Dutra <mdutra11@gmail.com>
2018-04-02 13:50:13 -0300

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+	// slider
+	$('.slider-testimonials').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		responsive: [
+	    {
+	      breakpoint: 991,
+	      settings: {
+	        slidesToShow: 1,
+	        slidesToScroll: 1
+	      }
+	    }
+	 	]
+	});
+
+	// menu mobile
+
+	var btnMenu = $('.hamburger-box');
+	var header = $('header');
+
+	btnMenu.click(function(){
+		console.log(btnMenu);
+		if(header.hasClass('is-expanded')){
+			header.removeClass('is-expanded');
+			$('body').removeClass('no-scroll');
+		}else{
+			header.addClass('is-expanded');
+			$('body').addClass('no-scroll');
+		}
+	});
+});
